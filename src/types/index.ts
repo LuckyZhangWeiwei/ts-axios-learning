9b6db7321e11188f6565058c08ba0a161ebc28dd
zhangweiwei <zhangzhang_10@hotmail.com>
2021-01-19 14:56:45 +0800

    init

---
+++ src/types/index.ts
@@ -0,0 +1,14 @@
+export type Method = 'get' | 'GET' 
+| 'delete' | 'DELETE'
+| 'post' | 'POST'
+| 'put' | 'PUT'
+| 'patch' | 'PATCH'
+| 'head' | 'HEAD' 
+| 'options' | 'OPTIONS'
+
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+}
